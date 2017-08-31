e331106a4bddbbd9fcb9ffb405ed0d1fa147d11d
vishalsanfran <vkhawarey@maprtech.com>
2017-08-31 12:58:10 -0700

    printer actor /w receive override

--- src/main/scala/com/lightbend/akka/sample/AkkaHelloWorld.scala
+++ src/main/scala/com/lightbend/akka/sample/AkkaHelloWorld.scala
@@ -1,6 +1,6 @@
 package com.lightbend.akka.sample
 
-import akka.actor.{ActorRef, Props, Actor}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 
 //companion object defines msgs handled by Greeter actor
 object Greeter {
@@ -13,6 +13,7 @@ object Greeter {
 //greeter actor
 class Greeter(message: String, printerActor: ActorRef) extends Actor {
   import Greeter._
+  import Printer._
 
   var greeting = ""
 
@@ -21,5 +22,21 @@ class Greeter(message: String, printerActor: ActorRef) extends Actor {
       greeting = s"$message, $who"
     case Greet =>
       printerActor ! Greeting(greeting)
+  }
+}
+
+//printer actor comp obj expects msg of type greeting
+object Printer {
+  def props: Props = Props[Printer]
+  final case class Greeting(greeting: String)
+}
+
+//printer actor
+class Printer extends Actor with ActorLogging {
+  import Printer._
+
+  def receive: Receive = {
+    case Greeting(greeting) =>
+      log.info(s"Greeting received (from ${sender()}): $greeting")
   }
 }
