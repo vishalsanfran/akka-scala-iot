356ff32ce09a036d86f167e192fd83e113faf18e
vishalsanfran <vkhawarey@maprtech.com>
2017-08-31 11:56:38 -0700

    props params for grtr actor

--- src/main/scala/com/lightbend/akka/sample/AkkaHelloWorld.scala
+++ src/main/scala/com/lightbend/akka/sample/AkkaHelloWorld.scala
@@ -1,7 +1,10 @@
 package com.lightbend.akka.sample
 
+import akka.actor.{ActorRef, Props}
+
 //companion object defines msgs handled by Greeter actor
 object Greeter {
+  def props(message: String, printerActor: ActorRef): Props = Props(new Greeter(message, printerActor))
   final case class WhoToGreet(who: String)
   case object Greet
 }
