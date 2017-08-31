79d4569655fb2af9c876cc552ee930570586ff87
vishalsanfran <vkhawarey@maprtech.com>
2017-08-31 13:50:08 -0700

    create test greeter, msg

--- src/test/scala/com/lightbend/akka/sample/AkkaHelloWorldSpec.scala
+++ src/test/scala/com/lightbend/akka/sample/AkkaHelloWorldSpec.scala
@@ -1,10 +1,16 @@
 package com.lightbend.akka.sample
 
 import akka.actor.ActorSystem
-import akka.testkit.TestKit
+import akka.testkit.{TestKit, TestProbe}
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 
 class AkkaHelloWorldSpec(system: ActorSystem) extends TestKit(system)
   with Matchers with FlatSpecLike with BeforeAndAfterAll {
+
+  "Greeter actor" should "pass greeting msg" in {
+    val testProbe = TestProbe()
+    val hiGreetingMsg = "hi"
+    val hiGreeter = system.actorOf(Greeter.props(hiGreetingMsg, testProbe.ref))
+  }
 
 }
