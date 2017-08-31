359a59d6093abe168eb24a1b549dc9143c3e582a
vishalsanfran <vkhawarey@maprtech.com>
2017-08-31 11:52:40 -0700

    companion object for greeter actor

--- src/main/scala/com/lightbend/akka/sample/AkkaHelloWorld.scala
+++ src/main/scala/com/lightbend/akka/sample/AkkaHelloWorld.scala
@@ -1,5 +1,12 @@
 package com.lightbend.akka.sample
 
+//companion object defines msgs handled by Greeter actor
+object Greeter {
+  final case class WhoToGreet(who: String)
+  case object Greet
+}
+
+
 class AkkaHelloWorld {
 
 }
