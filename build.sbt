cccd70ebc5df32a70f0ab00e8ba45242ea3100c0
vishalsanfran <vkhawarey@maprtech.com>
2017-08-31 14:06:35 -0700

    test msg rcv, add test libs

--- build.sbt
+++ build.sbt
@@ -4,5 +4,7 @@ scalaVersion := "2.12.2"
 lazy val akkaVersion = "2.5.3"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % akkaVersion
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-testKit" % akkaVersion,
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 )
--- src/test/scala/com/lightbend/akka/sample/AkkaHelloWorldSpec.scala
+++ src/test/scala/com/lightbend/akka/sample/AkkaHelloWorldSpec.scala
@@ -2,15 +2,28 @@ package com.lightbend.akka.sample
 
 import akka.actor.ActorSystem
 import akka.testkit.{TestKit, TestProbe}
+import Greeter._
+import Printer._
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+import scala.concurrent.duration._
 
-class AkkaHelloWorldSpec(system: ActorSystem) extends TestKit(system)
+class AkkaHelloWorldSpec(_system: ActorSystem) extends TestKit(_system)
   with Matchers with FlatSpecLike with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("akkaHelloWorldSpec"))
+
+  override def afterAll(): Unit = {
+    shutdown(system)
+  }
 
   "Greeter actor" should "pass greeting msg" in {
     val testProbe = TestProbe()
-    val hiGreetingMsg = "hi"
+    val hiGreetingMsg = "Hi There"
     val hiGreeter = system.actorOf(Greeter.props(hiGreetingMsg, testProbe.ref))
+    val greetTo = "Planet"
+    hiGreeter ! WhoToGreet(greetTo)
+    hiGreeter ! Greet
+    testProbe.expectMsg(500 millis, Greeting(s"$hiGreetingMsg, $greetTo"))
   }
 
 }
